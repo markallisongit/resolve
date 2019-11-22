bf4c3bd71c5bf2999c6d404723ee2ca2deb51523
Mark Allison <Mark.Allison@hee.nhs.uk>
2019-11-22 20:00:19 +0000

    Get status bar working

--- src/popup/getaddress.js
+++ src/popup/getaddress.js
@@ -2,16 +2,21 @@ window.onload = function () {
     // return key submits
     document.getElementById("handletext").addEventListener("keyup", function (e) {
         event.preventDefault();
-        if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
-        if (typeof(Storage) !== "undefined") {
-            var userinput = document.getElementById('handletext').value.trim();
-            localStorage.setItem("handle", userinput);
-            getAddress(userinput);
-            var head = document.getElementsByTagName("head")[0].innerHTML;
-            if (head.indexOf("moneybutton") === -1) {
-                loadMoneyButton();
-            }            
-        }
+        if ((event.keyCode > 47 && event.keyCode < 112) || event.keyCode === 8 || event.keyCode === 46) {
+            document.getElementById('outputbox').innerHTML = null;
+            document.getElementById('qrcode').innerHTML = null;
+
+        } else if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+            updateStatus("Loading");
+            if (typeof (Storage) !== "undefined") {
+                var userinput = document.getElementById('handletext').value.trim();
+                localStorage.setItem("handle", userinput);
+                getAddress(userinput);
+                var head = document.getElementsByTagName("head")[0].innerHTML;
+                if (head.indexOf("moneybutton") === -1) {
+                    loadMoneyButton();
+                }
+            }
             document.getElementById('handletext').value.trim();
         }
     });
@@ -28,9 +33,27 @@ window.onload = function () {
     document.getElementById("handletext").focus();
 }
 
-function getAddress (handle) {
+function updateStatus (message, type) {
+    var alertClass;
+    switch (type) {
+        case 'error':
+            alertClass = "alert-danger";
+            break;
+        case 'warning':
+            alertClass = "alert-warning";
+            break;
+        default:
+            alertClass = "alert-info";
+    }
+
+    document.getElementById('status').innerHTML = message;
+    document.getElementById('status').classList.add("alert");
+    document.getElementById('status').classList.add(alertClass);
+
+}
+function getAddress(handle) {
     var req = new XMLHttpRequest();
-    req.onreadystatechange = function() {
+    req.onreadystatechange = function () {
         if (req.readyState === 4) {
             if (req.status === 200) {
                 var response = req.responseText;
@@ -40,11 +63,11 @@ function getAddress (handle) {
                 document.getElementById('outputbox').innerHTML = address;
             }
             else if (req.status === 404) {
-                updateStatus ('$handle does not exist', 'error');
+                updateStatus('$handle does not exist', 'error');
                 return;
             }
             else {
-                updateStatus (req.statusText, 'error');
+                updateStatus(req.statusText, 'error');
                 return;
             }
         }
@@ -56,18 +79,18 @@ function getAddress (handle) {
 
 function loadMoneyButton() {
     var x = document.createElement("script");
-    x.src="https://api.moneybutton.com/moneybutton.js";
+    x.src = "https://api.moneybutton.com/moneybutton.js";
     document.getElementsByTagName("head")[0].appendChild(x); // Insert MB script into document head
-    setTimeout(function(){renderMoneyButton("0.25")}, 100);
+    setTimeout(function () { renderMoneyButton("0.25") }, 100);
 }
 
 function renderMoneyButton(amount) {
-    if(typeof(moneyButton) === "undefined") {
-        setTimeout(function(){renderMoneyButton(amount);}, 100);
+    if (typeof (moneyButton) === "undefined") {
+        setTimeout(function () { renderMoneyButton(amount); }, 100);
         return; // If MoneyButton isn't defined, try again in 100 ms
     }
-    if(typeof(moneyButton.render) === "undefined") {
-        setTimeout(function(){renderMoneyButton(amount);}, 100);
+    if (typeof (moneyButton.render) === "undefined") {
+        setTimeout(function () { renderMoneyButton(amount); }, 100);
         return; // If render isn't defined, try again in 100 ms
     }
     const div = document.getElementById('moneyButton')
@@ -83,4 +106,4 @@ function renderMoneyButton(amount) {
         type: "tip",
         editable: false
     }); // Render MB
-  }
+}
