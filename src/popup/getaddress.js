d061febbcfcd00a3188c4d3555e75e40885e6ec2
Mark Allison <Mark.Allison@hee.nhs.uk>
2019-11-24 17:07:59 +0000

    Release candidate

--- src/popup/getaddress.js
+++ src/popup/getaddress.js
@@ -1,43 +1,37 @@
 window.onload = function () {
+    hideDiv('status');
+    hideDiv('address');
+    var head = document.getElementsByTagName("head")[0].innerHTML;
+    if (head.indexOf("moneybutton") === -1) {
+        console.log("Loading Moneybutton");
+        loadMoneyButton();
+    }    
     // return key submits
     document.getElementById("handletext").addEventListener("keyup", function (e) {
-        document.getElementById('status').style.display='none';  
-        event.preventDefault();
-        if ((event.keyCode > 47 && event.keyCode < 112) || event.keyCode === 8 || event.keyCode === 46) {
-            document.getElementById('outputbox').innerHTML = null;
-            document.getElementById('qrcode').innerHTML = null;
-
-        } else if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
-            document.getElementById('status').style.display='block'
+        e.preventDefault();
+        if ((e.keyCode > 47 && e.keyCode < 112) || e.keyCode === 8 || e.keyCode === 46) {
+            hideDiv('address');
+        } else if (e.keyCode === 13) {  // Enter pressed   
             updateStatus("Resolving...", 'info');
             if (typeof (Storage) !== "undefined") {
                 var userinput = document.getElementById('handletext').value.trim();
                 localStorage.setItem("handle", userinput);
-                getAddress(userinput);
-                var head = document.getElementsByTagName("head")[0].innerHTML;
-                if (head.indexOf("moneybutton") === -1) {
-                    loadMoneyButton();
-                }
             }
+            getAddress(userinput);
             document.getElementById('handletext').value.trim();
         }
     });
-
-    // get the handle saved in storage and populate textbox with it, and load photo
+    // get the handle saved in storage and populate textbox with it
     if (typeof (Storage) != null) {
         var handle = this.localStorage.getItem("handle");
         if (handle != null) {
             document.getElementById("handletext").value = handle;
-            // getPhoto(handle);
         }
     }
-
     document.getElementById("handletext").focus();
 }
 
 function updateStatus (message, type) {
-    console.log(message);
-    console.log(type);
     document.getElementById('status').classList.remove("alert-danger");
     var alertClass;
     switch (type) {
@@ -56,8 +50,8 @@ function updateStatus (message, type) {
     }
 
     document.getElementById('status').innerHTML = message;
-    // document.getElementById('status').classList.add("alert");
     document.getElementById('status').classList.add(alertClass);
+    document.getElementById('status').style.display=('block');
 }
 
 function getAddress(handle) {
@@ -67,30 +61,25 @@ function getAddress(handle) {
     req.onreadystatechange = function () {
         if (req.readyState === 4) {
             var response = req.responseText;
-            console.log(response);
             var json = JSON.parse(response);
             address = json['address'];
             error = json['error'];
-            console.log(address);
-            console.log(error);
 
             if (req.status === 200) {
+                hideDiv('status');
                 if (address) {
                     document.getElementById('qrcode').innerHTML = "<img src=\"https://api.qrserver.com/v1/create-qr-code/?data=" + address + "&amp;size=150x150\" /img>"
                     document.getElementById('outputbox').innerHTML = address;                      
-                    document.getElementById('status').style.display='none';
-
+                    document.getElementById('address').style.display=('block');
                 } else  {
-                    console.log(error);
                     updateStatus(error, 'error');    
                 }             
             }
-            else if (req.status === 400) {
+            else if (req.status === 400 || req.status === 404) {
                 updateStatus(error, 'error');
                 return;
             }
             else {
-                // {"error":"$handle not found"}
                 updateStatus(req.statusText, 'error');
                 return;
             }
@@ -98,6 +87,14 @@ function getAddress(handle) {
     };
     req.open('GET', 'https://api.polynym.io/getAddress/' + handle);
     req.send(null);
+}
+
+function hideDiv (div){
+    document.getElementById(div).style.display=('none');
+}
+
+function clearOutput (){
+    document.getElementById('address').style.display=('none');
 }
 
 function loadMoneyButton() {
@@ -116,14 +113,14 @@ function renderMoneyButton(amount) {
         setTimeout(function () { renderMoneyButton(amount); }, 100);
         return; // If render isn't defined, try again in 100 ms
     }
-    const div = document.getElementById('moneyButton')
+    const div = document.getElementById('moneybutton')
     moneyButton.render(div, {
         amount: amount,
-        to: "271",
-        currency: "USD",
+        to: "marquee@moneybutton.com",
         amount: "0.02",
-        label: "Tip Marquee",
-        clientIdentifier: "4819bb79f7c579c20a14fd761b2192aa",
+        currency: "USD",
+        label: "Tip",
+        clientIdentifier: "4f07e9e4d6322bed4a8bcffe5aeb9ce4",
         buttonId: "1539184004542",
         buttonData: "{}",
         type: "tip",
